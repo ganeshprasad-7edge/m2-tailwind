57c7c16d5560e0ec79218679a07a5ea489ff8d49
Ganesh Prasad <gprasad@7edge.com>
2025-07-14 00:05:09 +0530

    completed m2 light and dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        jobView: '#F2EFFF',
+        jobApplied: '#5832E6',
+        searchbar: '#F6F6F6',
+        searchbarText: '#A7A7A7',
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
